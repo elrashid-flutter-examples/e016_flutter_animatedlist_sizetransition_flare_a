0fb45bbe776d6346489c2cf6e1cecb19d024b4eb
moh <osman.elrashid@gmail.com>
2019-08-04 06:04:45 +0400

    initial files

---
+++ android/app/src/main/java/com/example/e016_flutter_animatedlist_sizetransition_flare_a/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.e016_flutter_animatedlist_sizetransition_flare_a;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ git.init.bat
@@ -0,0 +1,5 @@
+Set HUB_PROTOCOL=ssh 
+git init 
+git add . && git commit -m "initial files" 
+hub create elrashid-flutter-examples/e016_flutter_animatedlist_sizetransition_flare_a 
+git push --set-upstream origin master 
